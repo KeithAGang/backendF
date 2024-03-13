ed99b0b15af1b56a1f54a8e58f52bdbedda9913b
KeithAGang <keithgangarahwe@gmail.com>
2024-03-13 20:08:50 +0200

    Fixed connection issues

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 #main.py file
 from fastapi import FastAPI, Depends, HTTPException
+from fastapi.middleware.cors import CORSMiddleware
 import fastapi.security as _security
 from typing import List
 from schemas import *
@@ -12,7 +13,22 @@ models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-@app.post("/users")
+# Define a list of allowed origins
+origins = [
+    "http://localhost:3000",
+    "http://localhost:8000",
+    "https://localhost:5000"
+]
+
+# Configure CORS middleware with allowed origins, methods, and headers
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
+
+@app.post("/api/users")
 async def create_user(user: UserCreate, db: _orm.Session = Depends(services.get_db)):
     db_user = await services.get_user_by_email(user.email, db)
     
@@ -24,7 +40,7 @@ async def create_user(user: UserCreate, db: _orm.Session = Depends(services.get_
     return await services.create_token(created_user)
 
 
-@app.post("/token")
+@app.post("/api/token")
 async def generate_token(form_data: _security.OAuth2PasswordRequestForm = Depends(), db: _orm.Session = Depends(services.get_db)):
     user = await services.authenticate_user(form_data.username, form_data.password, db)
     
@@ -33,7 +49,7 @@ async def generate_token(form_data: _security.OAuth2PasswordRequestForm = Depend
     
     return await services.create_token(user)
 
-@app.get("/users/me", response_model=Users)
+@app.get("/api/users/me", response_model=Users)
 async def get_user(current_user: Users = Depends(services.get_current_user)): # Update the dependency name
     return current_user
 
@@ -41,34 +57,37 @@ async def get_user(current_user: Users = Depends(services.get_current_user)): # 
 # async def create_message(message: MessageCreate, user: Users = Depends(services.get_current_user), db: _orm.Session = Depends(services.get_db)):
 #     return await services.generate_message(user=user, db=db, message=message)
 
-@app.get("/notifications", response_model=List[Notification])
+@app.get("/api/notifications", response_model=List[Notification])
 async def get_notifications(user: Users = Depends(services.get_current_user), db: _orm.Session = Depends(services.get_db)):
     return await services.generate_notification(user=user, db=db)
 
-@app.get("/notification/{msg_id}", status_code=200)
+@app.get("/api/notification/{msg_id}", status_code=200)
 async def get_specific_notification(msg_id: str, user: Users = Depends(services.get_current_user), db: _orm.Session = Depends(services.get_db)):
     return await services.get_notification(msg_id, user, db)
 
-@app.get("/my_account", response_model=List[AccountBalance])
+@app.get("/api/my_account", response_model=List[AccountBalance])
 async def get_my_account(user: Users = Depends(services.get_current_user), db:_orm.Session = Depends(services.get_db)):
     return await services.get_account(user=user, db=db)
 
-@app.put("/pay_now/{amount}", status_code=200)
+@app.put("/api/pay_now/{amount}", status_code=200)
 async def pay_for_parking_now(amount: float, user: Users = Depends(services.get_current_user), db: _orm.Session = Depends(services.get_db)):
     return await services.pay_for_Lot(amount=amount, user=user, db=db)    
 
-@app.post("/book_parking/{hours}", status_code=200)
+@app.post("/api/book_parking/{hours}", status_code=200)
 async def book_parking_lot(hours: int, immediate_booking: bool = True, user: UserDetails = Depends(services.get_current_user), db: _orm.Session = Depends(services.get_db)):
     return await services.book_parking_lot(user=user, db=db, hours=hours, immediate_booking=immediate_booking)
 
-@app.get("/revoke_my_resrvation")
+@app.get("/api/revoke_my_resrvation")
 async def revoke_my_parking_lot(user: UserDetails = Depends(services.get_current_user), db: _orm.Session = Depends(services.get_db)):
     return await services.revoke_my_reservation(user=user, db=db)
 
-@app.get("/my_parkinglot")
+@app.get("/api/my_parkinglot")
 async def get_me_parking_lot(user: UserDetails = Depends(services.get_current_user), db: _orm.Session = Depends(services.get_db)):
     return await services.lot_selector(user=user, db=db)
 
+@app.get("/api")
+async def root():
+    return {"message": "Success!"}
 
 #if you want to add lots to the db, uncommnet the code below
 # @app.get("/populate_lots")
